8fb83e46da89040fce1f5b7d976150f3a49bdd9d
Jungsun Park <clicksun@gmail.com>
2017-11-25 16:31:59 -0600

    soliution to 1.1

--- chapter01/1.1-isUnique.js
+++ chapter01/1.1-isUnique.js
@@ -1,15 +1,28 @@
+
 // uses hash table
-function isUnique (str) {
-  const charTable = {};
-  for (let i = 0; i < str.length; i++) {
-    if (charTable[str[i]] === 1) {
-      return false;
-    } else {
-      charTable[str[i]] = 1;
-    }
+// const isUnique = str => {
+//   const charTable = {};
+//   for (let i = 0; i < str.length; i++) {
+//     if (charTable[str[i]] === 1) {
+//       return false;
+//     } else {
+//       charTable[str[i]] = 1;
+//     }
+//   }
+//   return true;
+// };
+// run time: O(n)
+// space complexity: O(n)
+
+// without any other DS
+const isUnique = str => {
+  for (let i = 0; i < str.length - 1; i++) {
+    if (str.slice(i+1).indexOf(str[i]) > -1) return false;
   }
   return true;
-}
+};
+// run time: O(n^2)
+// space complexity: O(1)
 
 console.log(isUnique('abc')); // true
 console.log(isUnique('aabc')); // false
