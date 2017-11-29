c6bb04bd0ab1a660ec1f8625ea35e15a7c46d207
Jungsun Park <clicksun@gmail.com>
2017-11-29 13:10:16 -0600

    build order using graphs

---
+++ chapter04/4.7-buildOrder.js
@@ -0,0 +1,34 @@
+// directed graph
+var Graph = require('./utils/Graph');
+
+// Solution - DFS
+const nodeWithNoChild = (graph) => {
+  for (let node in graph.nodes) {
+    if (Object.keys(graph.findEdges(node)).length < 1) return node;
+  }
+};
+
+const buildOrder = (projects, dependencies) => {
+  const buildOrderArr = [];
+
+  // Build graph
+  const graph = new Graph();
+  projects.forEach(proj => graph.addNode(proj));
+  dependencies.forEach(dep => graph.addEdge(dep[0], dep[1]));
+
+  // Start putting leaves into build order
+  let curNode = nodeWithNoChild(graph);
+  while (curNode) {
+    buildOrderArr.unshift(curNode);
+    graph.removeNode(curNode);
+    curNode = nodeWithNoChild(graph);
+  }
+
+  return (buildOrderArr.length === projects.length) ? buildOrderArr : Error;
+};
+
+/* TEST */
+var projects = ['a', 'b', 'c', 'd', 'e', 'f'];
+var dependencies = [['a', 'd'], ['f', 'b'], ['b', 'd'], ['f', 'a'], ['d', 'c']];
+
+console.log(buildOrder(projects, dependencies));
