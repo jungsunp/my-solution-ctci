999082525e5c24b9b8bb92408926eb0273fed2db
Jungsun Park <clicksun@gmail.com>
2017-12-14 13:38:53 -0600

    symmetric tree check

---
+++ Leetcode/symmetricTree.js
@@ -0,0 +1,58 @@
+
+
+// https://leetcode.com/problems/symmetric-tree/description/
+
+// Solution - BFS (Iterative)
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+// var isSymmetric = function(root) {
+//   if (!root) return true;
+//   let leftArr = [root.left];
+//   let rightArr = [root.right];
+//   let allNull = (root.left === null) && (root.right === null);
+
+//   while(!allNull) {
+//       allNull = true;
+//       const tmpLeftArr = [];
+//       const tmpRightArr = [];
+
+//       for (let i = 0; i < leftArr.length; i++){
+//           const j = rightArr.length - i - 1;
+//           const leftVal = leftArr[i] ? leftArr[i].val : null;
+//           const rightVal = rightArr[j] ? rightArr[j].val : null;
+//           if (leftVal !== rightVal) return false;
+
+//           if (leftVal !== null) allNull = false;
+//           else continue;
+
+//           tmpLeftArr.push(leftArr[i].left, leftArr[i].right);
+//           tmpRightArr.unshift(rightArr[j].left, rightArr[j].right);
+//       }
+
+//       leftArr = tmpLeftArr;
+//       rightArr = tmpRightArr;
+//   }
+
+//   return true;
+// };
+
+// Recursive - DFS
+const symmetricHelper = (lNode, rNode) => {
+  if (!lNode || !rNode) return lNode === rNode;
+  if (lNode.val !== rNode.val) return false;
+  return symmetricHelper(lNode.left, rNode.right) && symmetricHelper(lNode.right, rNode.left);
+}
+
+var isSymmetric = function(root) {
+  if (!root) return true;
+  return symmetricHelper(root.left, root.right);
+};
