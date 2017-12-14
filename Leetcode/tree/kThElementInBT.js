929abfbeeb6cbbc0d10fb9b0b1f0635a9c016a21
Jungsun Park <clicksun@gmail.com>
2017-12-14 15:46:28 -0600

    finding kth element in BST using DFS

---
+++ Leetcode/tree/kThElementInBT.js
@@ -0,0 +1,30 @@
+
+// https://leetcode.com/problems/kth-smallest-element-in-a-bst/description/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
+// DFS - iterative
+var kthSmallest = function(root, k) {
+  const nodeStack = [];
+  let node = root;
+  while (node || nodeStack.length > 0) {
+      while (node) {
+          nodeStack.push(node);
+          node = node.left;
+      }
+      node = nodeStack.pop();
+      if (--k === 0) break;
+      node = node.right;
+  }
+  return node.val;
+};
