07f0f5cc2ca6e8b834b20bff907f839f2f2175ae
Jungsun Park <clicksun@gmail.com>
2017-12-15 13:58:46 -0600

    copt list with random pointer from LL

---
+++ Leetcode/linkedList/copyListWithRandomPointer.js
@@ -0,0 +1,69 @@
+
+
+// https://leetcode.com/problems/copy-list-with-random-pointer/description/
+
+// hash-map
+// var copyRandomList = function(head) {
+//   if (!head) return null;
+
+//   // copy all nodes to hash
+//   const hash = new Map();
+//   let node = head;
+//   while (node) {
+//       hash.set(node, new RandomListNode(node.label));
+//       node = node.next;
+//   }
+
+//   // set pointers
+//   node = head;
+//   let newNode = hash.get(node);
+//   while (node.next) {
+//       newNode.next = hash.get(node.next);
+//       newNode.random = node.random ? hash.get(node.random) : null;
+//       node = node.next;
+//       newNode = newNode.next;
+//   }
+
+//   return hash.get(node);
+// };
+// Run: O(n)
+// Space: O(n)
+
+// less space used
+var copyRandomList = function(head) {
+  if (!head) return null;
+
+  // copy to the original list
+  let node = head;
+  while (node) {
+      const next = node.next;
+      node.next = new RandomListNode(node.label);
+      node.next.next = next;
+      node = next;
+  }
+
+  // set random pointers
+  node = head;
+  while (node) {
+      if (node.random) {
+          node.next.random = node.random.next;
+      }
+      node = node.next.next;
+  }
+
+  // extract new list
+  const dummyNode = new RandomListNode();
+  node = head;
+  let newNode = dummyNode;
+  while (node) {
+      const next = node.next.next;
+      newNode.next = node.next;
+      newNode = newNode.next;
+      node.next = next;
+      node = next;
+  }
+
+  return dummyNode.next;
+};
+// Run: O(n)
+// Space: O(1)
