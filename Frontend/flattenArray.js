ad23e3de107deb8668f323bf99429924bf27fa3a
Jungsun Park <clicksun@gmail.com>
2017-12-17 20:37:18 -0600

    flatten array iteratively

--- Frontend/flattenArray.js
+++ Frontend/flattenArray.js
@@ -16,13 +16,27 @@
 // Space: O(n)
 
 // Solution ES6
-const flattenArr = arr => arr.reduce(
-  (a, b) => a.concat(Array.isArray(b) ? flattenArr(b) : b), []
-);
+// const flattenArr = arr => arr.reduce(
+//   (a, b) => a.concat(Array.isArray(b) ? flattenArr(b) : b),
+//   []
+// );
+
+// itertative solution
+const flattenArr = arr => {
+  let index = 0;
+  while (index < arr.length) {
+    if (Array.isArray(arr[index])) {
+      arr.splice(index, 1, ...arr[index]);
+    } else {
+      index++;
+    }
+  }
+  return arr;
+};
 
 console.log(flattenArr([]));
-console.log(flattenArr([1,2,3,4]));
-console.log(flattenArr([1,[2,3],4]));
-console.log(flattenArr([1,2,[3,[4,5,[6]]],7]));
-console.log(flattenArr([1,2,[3,[4,5,[6]],[7,8]],9]));
+console.log(flattenArr([1, 2, 3, 4]));
+console.log(flattenArr([1, [2, 3], 4]));
+console.log(flattenArr([1, 2, [3, [4, 5, [6]]], 7]));
+console.log(flattenArr([1, 2, [3, [4, 5, [6]], [7, 8]], 9]));
 console.log(flattenArr([[0, 1], [2, 3], [4, 5, [6, 7, [8, [9, 10]]]]]));
