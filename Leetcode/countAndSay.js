f6f59a464ca57b70f43ccfe76bc63268cba1f1ba
Jungsun Park <clicksun@gmail.com>
2017-12-29 13:07:20 -0600

    count and say string generator

---
+++ Leetcode/countAndSay.js
@@ -0,0 +1,27 @@
+
+
+// https://leetcode.com/problems/count-and-say/description/
+
+/**
+ * @param {number} n
+ * @return {string}
+ */
+var countAndSay = function(n) {
+  if (n <= 1) return '1';
+
+  let retStr = '';
+  const prevStr = countAndSay(n-1);
+  let index = 0;
+  while (index < prevStr.length) {
+      let count = 1;
+      while (prevStr[index] === prevStr[index + 1]) {
+          count++;
+          index++;
+      }
+      retStr += count + prevStr[index];
+      index++;
+  }
+  return retStr;
+};
+// Run: O(n ^ 2) (?)
+// Space: O(n)
