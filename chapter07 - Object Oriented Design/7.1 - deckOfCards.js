d7b9151fe829e678f3d0c60850fe6a6a4a58e85d
Jungsun Park <clicksun@gmail.com>
2018-01-04 16:20:14 -0600

    initial blackjack game setup and fix arrow functions for this problems

--- chapter07 - Object Oriented Design/7.1 - deckOfCards.js
+++ chapter07 - Object Oriented Design/7.1 - deckOfCards.js
@@ -2,23 +2,23 @@
 
 // Deck & Card
 
-function Card (suit, num) {
+function Card(suit, num) {
   this.suit = suit;
   this.num = num;
 }
 
-Card.prototype.gameValue = () => {
+Card.prototype.gameValue = function () {
   if (typeof this.num === 'number') return this.num;
   if (this.num === 'A') return [1, 11];
   return 10;
 };
 
-function Deck () {
+function Deck() {
   this.cards = [];
   this.newDeck();
 }
 
-Deck.prototype.newDeck = () => {
+Deck.prototype.newDeck = function () {
   const suits = ['diamond', 'clover', 'hert', 'spade'];
   const nums = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'];
 
@@ -29,48 +29,107 @@ Deck.prototype.newDeck = () => {
   });
 };
 
-Deck.prototype.clear = () => {
+Deck.prototype.clear = function () {
   this.cards.length = 0;
 };
 
-Deck.prototype.shuffle = () => {
-  this.cards.sort(() => Math.random() > 0.5 ? 1 : -1 );
+Deck.prototype.shuffle = function () {
+  this.cards.sort(() => Math.random() > 0.5 ? 1 : -1);
 };
 
-Dock.prototype.deal = () => {
+Deck.prototype.deal = function () {
   return this.cards.pop();
 };
 
 // Dealer & Player
 
-function Dealer () {
+function Dealer() {
   this.deck = new Deck();
   this.hands = [];
 }
 
-Dealer.prototype.shuffle = () => {
+Dealer.prototype.shuffle = function () {
   this.deck.shuffle();
 };
 
-Dealer.prototype.deal = () => {
+Dealer.prototype.deal = function () {
   return this.deck.deal();
 };
 
-Dealer.prototype.receiveCard = card => {
+Dealer.prototype.receiveCard = function (card) {
   this.hands.push(card);
+  console.log(`Dealer received ${card}`);
 };
 
-function Player () {
+function Player(name) {
+  this.name = name;
   this.hands = [];
 }
 
-Player.prototype.receiveCard = card => {
+Player.prototype.receiveCard = function (card) {
   this.hands.push(card);
+  console.log(`Player ${this.name} received ${card}`);
 };
 
-Player.prototype.discardCard = () => {
+Player.prototype.discardCard = function () {
   this.hands.length = 0;
 };
 
 // Blackjack
 
+function GameTable() {
+  this.dealer = new Dealer();
+  this.players = [];
+}
+
+GameTable.prototype.newPlayer = function (name) {
+  if (this.players.length > 4) {
+    console.log('Game table is full');
+    return;
+  }
+  this.players.push(new Player(name));
+};
+
+GameTable.prototype.runGame = function () {
+  if (this.players.length < 1) {
+    console.log('No player in game table');
+    return;
+  }
+
+  console.log('***** Blackjack start! *****');
+  this.dealer.shuffle();
+
+  console.log('Cards are dealt to players');
+  this.players.forEach(player => {
+    player.receiveCard(this.dealer.deal());
+    player.receiveCard(this.dealer.deal());
+  });
+  console.log('Cards are dealt to dealer');
+  this.dealer.receiveCard(this.dealer.deal());
+  this.dealer.receiveCard(this.dealer.deal());
+
+  // let finish = false;
+  // while (!finish) {
+  //   this.players.forEach(player => {
+
+  //   });
+  // }
+
+  console.log('***** Blackjack finished! *****');
+};
+
+
+// TEST
+const table = new GameTable();
+const eugene = new Player();
+const david = new Player();
+const luis = new Player();
+const eric = new Player();
+
+table.newPlayer(eugene);
+table.newPlayer(david);
+table.newPlayer(luis);
+table.newPlayer(eric);
+
+/* build until dealing of first hand */
+table.runGame();
