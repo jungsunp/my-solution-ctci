880d8df94bfd64c86cb3bdb59266338bb9a390ab
Jungsun Park <clicksun@gmail.com>
2018-01-04 17:03:39 -0600

    OOD for blackjack game

--- chapter07 - Object Oriented Design/7.1 - deckOfCards.js
+++ chapter07 - Object Oriented Design/7.1 - deckOfCards.js
@@ -58,7 +58,28 @@ Dealer.prototype.deal = function () {
 
 Dealer.prototype.receiveCard = function (card) {
   this.hands.push(card);
-  console.log(`Dealer received ${card}`);
+  console.log(`Dealer received ${card.num}`);
+};
+
+const totalValue = hands => {
+  let sum = 0;
+  hands.forEach(card => {
+    const gameVal = card.gameValue();
+    if (typeof gameVal === 'number') {
+      sum += gameVal;
+    } else {
+      if (sum + gameVal[1] <= 21) {
+        sum += gameVal[1];
+      } else {
+        sum += gameVal[0];
+      }
+    }
+  });
+  return sum;
+};
+
+Dealer.prototype.totalValue = function () {
+  return totalValue(this.hands);
 };
 
 function Player(name) {
@@ -68,11 +89,15 @@ function Player(name) {
 
 Player.prototype.receiveCard = function (card) {
   this.hands.push(card);
-  console.log(`Player ${this.name} received ${card}`);
+  console.log(`Player ${this.name} received ${card.num}`);
 };
 
 Player.prototype.discardCard = function () {
   this.hands.length = 0;
+};
+
+Player.prototype.totalValue = function () {
+  return totalValue(this.hands);
 };
 
 // Blackjack
@@ -96,40 +121,75 @@ GameTable.prototype.runGame = function () {
     return;
   }
 
-  console.log('***** Blackjack start! *****');
+  console.log('***** Blackjack start *****');
   this.dealer.shuffle();
 
-  console.log('Cards are dealt to players');
+  console.log();
+  console.log('-- Cards are dealt to players --');
   this.players.forEach(player => {
     player.receiveCard(this.dealer.deal());
     player.receiveCard(this.dealer.deal());
   });
-  console.log('Cards are dealt to dealer');
+
+  console.log();
+  console.log('-- Cards are dealt to dealer --');
   this.dealer.receiveCard(this.dealer.deal());
   this.dealer.receiveCard(this.dealer.deal());
 
-  // let finish = false;
-  // while (!finish) {
-  //   this.players.forEach(player => {
+  let finish = false;
+  let winners = [];
+  const players = new Set();
+  this.players.forEach(player => players.add(player));
+  while (!finish && players.size) {
+    console.log();
+    console.log('-- A round starts --');
+    players.forEach(player => {
+      const tot = player.totalValue();
+      if (tot > 21) {
+        players.delete(player);
+      } else if (tot === 21) {
+        winners.push(player);
+        finish = true;
+      } else {
+        player.receiveCard(this.dealer.deal());
+      }
+    });
 
-  //   });
-  // }
+    const tot = this.dealer.totalValue();
+    if (tot > 21) {
+      winners = players;
+      finish = true;
+    } else if (tot === 21) {
+      finsih = true;
+    } else {
+      this.dealer.receiveCard(this.dealer.deal());
+    }
 
-  console.log('***** Blackjack finished! *****');
+    console.log('-- A round ends --');
+  }
+
+  console.log();
+  if (winners.length > 0) {
+    console.log('-- Players win! --');
+    winners.forEach(player => {
+      console.log(`Player ${player.name} won!`);
+    });
+  } else {
+    console.log('-- Dealer won! --');
+  }
+
+  console.log();
+  console.log('***** Blackjack finished *****');
 };
 
 
 // TEST
 const table = new GameTable();
-const eugene = new Player();
-const david = new Player();
-const luis = new Player();
-const eric = new Player();
 
-table.newPlayer(eugene);
-table.newPlayer(david);
-table.newPlayer(luis);
-table.newPlayer(eric);
+table.newPlayer('Jung');
+table.newPlayer('Jin');
+table.newPlayer('Eric');
+table.newPlayer('Hyunho');
 
 /* build until dealing of first hand */
 table.runGame();
