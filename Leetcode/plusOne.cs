9b1ec5edc58c793680e8933f2f62120cf8f5bb08
Jungsun Park <clicksun@gmail.com>
2018-01-12 17:39:46 -0600

    slight more optimized solution to plus one

--- Leetcode/plusOne.cs
+++ Leetcode/plusOne.cs
@@ -11,7 +11,7 @@ public class Solution {
           if (num < 10) {
             list.Insert(0, num);
             carry = 0;
-          } else { // num == 10
+          } else {
             list.Insert(0, 0);
             carry = 1;
           }
@@ -20,3 +20,26 @@ public class Solution {
         return list.ToArray();
     }
 }
+// Run: O(n)
+// Space: O(n)
+
+
+// Slightly more optimized solution
+public class Solution {
+    public int[] PlusOne(int[] digits) {
+        for (int i = digits.Length - 1; i >= 0; i--) {
+          int num = digits[i] + 1;
+          if (num <= 9) {
+            digits[i]++;
+            return digits;
+          } else { // num == 10
+            digits[i] = 0;
+          }
+        }
+        int[] retDigits = new int[digits.Length + 1];
+        retDigits[0] = 1;
+        return retDigits;
+    }
+}
+// Run: O(n)
+// Space: O(1) most case, O(n) worst case
