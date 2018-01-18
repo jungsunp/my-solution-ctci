859768028f52718b05e9dcdd178b71f975562b14
Jungsun Park <clicksun@gmail.com>
2018-01-17 21:21:07 -0600

    fix spacing from rotate image

--- Leetcode/rotateImage.cs
+++ Leetcode/rotateImage.cs
@@ -4,15 +4,14 @@
 // Note that this is same problem as CTCI - 1.7
 
 public class Solution {
+
     public void Rotate(int[,] matrix) {
         int length = matrix.GetLength(0); // number of row
-
         for (int row = 0; row < length / 2; row++) {
           for (int col = row; col < length - row - 1; col++) {
             MovePixel(matrix, row, col);
           }
         }
-
     }
 
     public void MovePixel(int[,] matrix, int row, int col) {
@@ -21,7 +20,6 @@ public class Solution {
       int toRow = col;
       int toCol = length - row - 1;
       int copyNum = matrix[row, col];
-
 
       for (int i = 0; i < 4; i++) {
         tmpNum = matrix[toRow, toCol];
@@ -33,7 +31,5 @@ public class Solution {
         toRow = fromCol;
         toCol = length - fromRow - 1;
       }
-
-
     }
 }
